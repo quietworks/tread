e481bdba857ac755a5dd0348e33a4d2a7bf45c2d
Josh Grant <jgrant@sms.com>
2025-11-26 05:29:28 -0800

    fix(ui): add article view scrolling support

--- src/ui/components/index.ts
+++ src/ui/components/index.ts
@@ -1,13 +1,9 @@
 export { FeedList, type FeedListProps, type FeedListItem } from "./FeedList.js";
 export { ArticleList, type ArticleListProps } from "./ArticleList.js";
-export {
-	ArticleView,
-	type ArticleViewProps,
-	type ArticleViewRef,
-	createArticleViewRef,
-} from "./ArticleView.js";
+export { ArticleView, type ArticleViewProps } from "./ArticleView.js";
 export { StatusBar, type StatusBarProps, type Pane } from "./StatusBar.js";
 export {
 	CommandPalette,
 	type CommandPaletteProps,
 } from "./CommandPalette.js";
+export type { ScrollBoxRenderable } from "@opentui/core";
